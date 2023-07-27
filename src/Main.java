8f85554c9a11d1577300ad642c12c7ee5ef15422
joseManuelAyala <119529931+joseManuelAyala@users.noreply.github.com>
2023-07-27 17:43:19 +0200

    anadimos cosas de programacion

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Cuantas veces quieres imprimir el numero?");
+        int h = 0;
+        try {
+            h = scanner.nextInt();
+        } catch (NumberFormatException e) {
+            System.out.println("El numero que haz dado no es valido");
+        }
+
+        for (int i = 0; i < h; i++) {
+            System.out.println("Victor me la pela");
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println("Me mama mi roomie");
+
+
+    }
+}
